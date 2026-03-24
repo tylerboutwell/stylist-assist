0771d48b49aa6f5a7ae87cca2a2048e1efafaf82
Tyler Boutwell <tylerboutwell1@gmail.com>
2026-03-24 17:25:12 -0500

    remove group view/serializer/url. not needed

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
-from django.contrib.auth.models import Group, User
+from django.contrib.auth.models import User
 from rest_framework import permissions, viewsets
 
-from .serializers import GroupSerializer, UserSerializer
+from .serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -11,14 +11,4 @@ class UserViewSet(viewsets.ModelViewSet):
 
     queryset = User.objects.all().order_by("-date_joined")
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-
-    queryset = Group.objects.all().order_by("name")
-    serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
